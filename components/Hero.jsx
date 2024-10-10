806b441af905d5ac8505980ce5a7c14200ecaa36
Aman Malik <amanmalik0135@gmail.com>
2024-10-10 21:00:35 +0530

    Updated files

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,9 @@ import SearchBox from './ui/SearchBox'
 
 const Hero = () => {
   return (
-<section className='pt-24 pb-12 xl:py-0 xl:h-[1887px] bg-[url(/hero/backgg.jpg)] xl:bg-[url(/hero/)] bg-cover bg-center bg-no-repeat flex justify-center items-center'>
+<section className='pt-24 pb-12 xl:py-0 xl:h-[1887px] flex justify-center relative items-center'>
+ <Image className='bg-cover absolute z-100  bg-center bg-no-repeat' src='./hero/backgg.jpg' width={1000}
+ height={1000} alt='did'/>
   <div className="container mx-auto flex items-center justify-center xl:justify-start">
     <SearchBox />
   </div>
