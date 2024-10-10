91be4adba83ff63ef05b3d884bec3b0ba2a7e0be
Aman Malik <amanmalik0135@gmail.com>
2024-10-10 21:03:58 +0530

    Updated files

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import SearchBox from './ui/SearchBox'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
-<section className='pt-24 pb-12 xl:py-0 xl:h-[1887px] flex justify-center relative items-center'>
- <Image className='bg-cover absolute z-100  bg-center bg-no-repeat' src='./hero/backgg.jpg' width={1000}
+<section className='pt-24 pb-12 xl:py-0 xl:h-[1887px] flex justify-center items-center'>
+ <Image className='bg-cover z-100 bg-center bg-no-repeat' src='./hero/backgg.jpg' width={1000}
  height={1000} alt='did'/>
   <div className="container mx-auto flex items-center justify-center xl:justify-start">
     <SearchBox />
