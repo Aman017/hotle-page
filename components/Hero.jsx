68646d1ca81b298b2afa5e6fd3b2017f3dd27644
Aman Malik <amanmalik0135@gmail.com>
2024-10-10 21:18:16 +0530

    Updated files

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,24 +1,20 @@
-import React from 'react'
-import SearchBox from './ui/SearchBox'
-import Image from 'next/image'
+import Image from 'next/image';
+import SearchBox from './SearchBox'; // Adjust path as necessary
 
-const Hero = () => {
+export default function HeroSection() {
   return (
-    <section className='relative pt-24 pb-12 xl:py-0 xl:h-[1887px] flex justify-center items-center'>
+    <section className='relative pt-24 pb-12 xl:py-0 xl:h-auto flex justify-center items-center'>
       {/* Image as a background */}
       <Image 
         className='absolute inset-0 -z-20 object-cover'
-        src='./hero/backgg.jpg'
+        src='./hero/backgg.jpg' // Use correct path
         layout='fill' 
         alt='Hero background' 
       />
       
-      <div className="container mx-auto flex items-center justify-center xl:justify-start relative z-10"> {/* Ensure the search box is on top */}
+      <div className="container mx-auto flex items-center justify-center relative z-10">
         <SearchBox />
       </div>
     </section>
   );
-};
-
-
-export default Hero
+}
