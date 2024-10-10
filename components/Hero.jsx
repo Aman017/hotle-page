6daaabf8241f6a947da6099c7da89ca40cfedc72
Aman Malik <amanmalik0135@gmail.com>
2024-10-10 21:08:45 +0530

    Updated files

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,15 +4,21 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-<section className='pt-24 pb-12 xl:py-0 xl:h-[1887px] flex justify-center items-center'>
- <Image className='bg-cover z-100 bg-center bg-no-repeat' src='./hero/backgg.jpg' width={1000}
- height={1000} alt='did'/>
-  <div className="container mx-auto flex items-center justify-center xl:justify-start">
-    <SearchBox />
-  </div>
-</section>
+    <section className='relative pt-24 pb-12 xl:py-0 xl:h-[1887px] flex justify-center items-center'>
+      {/* Image as a background */}
+      <Image 
+        className='absolute inset-0 -z-20 object-cover' // Use absolute positioning for the image
+        src='/hero/backgg.jpg' // Make sure the path is correct
+        layout='fill' // Use layout 'fill' to cover the section
+        alt='Hero background' 
+      />
+      
+      <div className="container mx-auto flex items-center justify-center xl:justify-start relative z-10"> {/* Ensure the search box is on top */}
+        <SearchBox />
+      </div>
+    </section>
+  );
+};
 
-  )
-}
 
 export default Hero
