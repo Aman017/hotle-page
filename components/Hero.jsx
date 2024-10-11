e02325823c0d2327c3fd4c41a329370e1f0b6adc
Aman Malik <amanmalik0135@gmail.com>
2024-10-11 15:44:42 +0530

    Updated feature or bug fix description

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,20 +4,20 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
- <section  >
-  <div className='pt-24 pb-12 xl:py-0 xl:h-[1887px] flex justify-center items-center'>
+ <section className='pt-24 pb-12 xl:py-0 xl:h-[1887px] flex justify-center items-center'>
+  
     <Image 
     src={'./hero/backgg.jpg'}
     alt='back'
     layout='fill'
     objectFit='cover'
     quality={100}
-    className='-z-10 bg-cover bg-center bg-no-repeat'
+    className='-z-1 bg-cover bg-center bg-no-repeat'
     />
   <div className="container mx-auto flex items-center justify-center xl:justify-start ">
     <SearchBox />
   </div>
-  </div>
+ 
  </section>
   )
 }
