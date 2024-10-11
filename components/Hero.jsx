a4da1a1d2a9c515c314fada15aa0cae1e3b2b00b
Aman Malik <amanmalik0135@gmail.com>
2024-10-11 16:45:55 +0530

    Description of your changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,6 +5,15 @@ const Hero = () => {
   return (
  <section className='pt-24 pb-12 xl:py-0 bg-[#2A6599] xl:h-[1000px] flex justify-center items-center' >
   <div className="container mx-auto flex items-center justify-center xl:justify-start ">
+    <div>
+      <Image 
+      src='./hero/hero.jpg'
+      width={100}
+      height={100}
+      alt='oop'
+      className='rounded-full right-20 '
+      />
+    </div>
     <SearchBox />
   </div>
  </section>
