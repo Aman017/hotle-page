2ec3172a24ace925810b72b30a7b5c61007df238
Aman Malik <amanmalik0135@gmail.com>
2024-10-11 17:05:11 +0530

    Description of your changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import SearchBox from './ui/SearchBox'
-import Image from 'next/image'
+import Image from 'next/image';
+import SearchBox from './SearchBox'; // Adjust this import based on your file structure
 
-const Hero = () => {
+const YourComponent = () => {
   return (
- <section className='pt-24 pb-12 xl:py-0 bg-[#2A6599] xl:h-[1000px] flex justify-center items-center' >
-  <div className="container mx-auto flex items-center justify-center xl:justify-start ">
-    <div className='m-8 p-5 relative'>
-      <Image 
-      src='./hero/hero.jpg'
-      width={800}
-      height={800}
-      alt='oop'
-      className='rounded-full '
-      />
-    </div>
-    <SearchBox />
-  </div>
- </section>
-  )
-}
+    <section className='pt-24 pb-12 xl:py-0 bg-[#2A6599] xl:h-[1000px] flex justify-center items-center'>
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Image Wrapper */}
+        <div className='relative flex-shrink-0'>
+          <Image 
+            src='/hero/hero.jpg' // Ensure this path is correct
+            width={800} // Default width
+            height={800} // Set height equal to width
+            alt='Profile Image' 
+            className='rounded-full object-cover w-32 h-32 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 xl:w-96 xl:h-96' 
+          />
+        </div>
+        {/* Search Box */}
+        <div className="flex-grow flex justify-end">
+          <SearchBox />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default YourComponent;
